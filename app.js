736a5202d85306cc5131bc789a91540df73af68d
Gabriel Nunes <gabrielconceicaonunes@gmail.com>
2024-09-05 10:59:49 -0300

    Implementado função de busca

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log(atletas[2].nome);
+function pesquisar() {
+    let section = document.getElementById("resultados-pesquisa");
+
+    let resultados = "";
+
+    for (let atleta of atletas ) {
+        resultados += `
+            <div class="item-resultado">
+                <h2>
+                    <a href="#" target="_blank">${atleta.nome}</a>
+                </h2>
+                <p class="descricao-meta">${atleta.desc}</p>
+                <a href="${atleta.link}" target="_blank">Mais informações</a>
+            </div>
+        `
+    }
+
+    section.innerHTML = resultados
+}
