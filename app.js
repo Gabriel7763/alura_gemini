c5fae09ef72a310bbbb7f47101113caa750b7614
Gabriel Nunes <gabrielconceicaonunes@gmail.com>
2024-09-06 11:08:38 -0300

    Implementando filtragem de busca

--- app.js
+++ app.js
@@ -1,19 +1,40 @@
 function pesquisar() {
     let section = document.getElementById("resultados-pesquisa");
+    let campoPesquisa = document.getElementById("campo-pesquisa").value;
 
-    let resultados = "";
-
-    for (let atleta of atletas ) {
-        resultados += `
-            <div class="item-resultado">
-                <h2>
-                    <a href="#" target="_blank">${atleta.nome}</a>
-                </h2>
-                <p class="descricao-meta">${atleta.desc}</p>
-                <a href="${atleta.link}" target="_blank">Mais informações</a>
-            </div>
-        `
+    if (!campoPesquisa) {
+        section.innerHTML = "<p>Nenhum atleta foi encontrado. Digite o nome de um atleta ou esporte</p>"
+        return
     }
 
-    section.innerHTML = resultados
+    campoPesquisa = campoPesquisa.toLowerCase()
+    let resultados = "";
+    let nome = "";
+    let desc = "";
+    let tags = "";
+
+    for (let atleta of atletas ) {
+        nome = atleta.nome.toLowerCase()
+        desc = atleta.desc.toLowerCase()
+        tags = atleta.tags.toLowerCase()
+        // se titulo inclui o campoPesquisa
+        if(nome.includes(campoPesquisa) || desc.includes(campoPesquisa) || tags.includes(campoPesquisa)) {
+            // cria um novo elemento
+            resultados += `
+                <div class="item-resultado">
+                    <h2>
+                        <a href="#" target="_blank">${atleta.nome}</a>
+                    </h2>
+                    <p class="descricao-meta">${atleta.desc}</p>
+                    <a href="${atleta.link}" target="_blank">Mais informações</a>
+                </div>
+            `
+        }
+        
+    };
+
+    if (!resultados) {
+        resultados = "<p>Nenhum atleta foi encontrado</p>"
+    }
+    section.innerHTML = resultados;
 }
--- dados.js
+++ dados.js
@@ -2,16 +2,19 @@ let atletas = [
     {
         nome: "Rayssa Leal",
         desc: "Rayssa Leal, conhecida como a 'Fadinha', é uma skatista brasileira que se tornou a mais jovem medalhista olímpica da história do Brasil. Aos 13 anos, conquistou a medalha de prata nos Jogos Olímpicos de Tóquio 2020.",
-        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal"
+        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        tags: "skate fada sk8"
     }, 
     {
         nome: "Beatriz Souza",
         desc: "Beatriz Souza é uma judoca brasileira que se destaca no cenário mundial. Com sua técnica apurada e determinação, ela conquistou diversos títulos e representou o Brasil em importantes competições internacionais.",
-        link: "https://www.google.com/search?q%"
+        link: "https://www.google.com/search?q%",
+        tags: "judô judo judoka"
     }, 
     {
         nome: "Rebeca Rodrigues",
         desc: "Rebeca Rodrigues de Andrade é, sem dúvida, um dos maiores nomes do esporte brasileiro. A ginasta, nascida em Guarulhos, São Paulo, em 1999, conquistou o coração dos brasileiros com suas performances impecáveis e sua determinação.",
         link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags: "ginástica ginastica ouro"
     }
-];
+]
